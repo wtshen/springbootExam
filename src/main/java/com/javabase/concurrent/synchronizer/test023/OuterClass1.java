b69cd3a9d842d17697497eb405c690f3083d924a
wtshen <hiswt@sina.com>
2018-03-26 21:19:27 +0800

    新增内部类例子

--- src/main/java/com/javabase/concurrent/synchronizer/test023/OuterClass1.java
+++ src/main/java/com/javabase/concurrent/synchronizer/test023/OuterClass1.java
@@ -1,16 +1,13 @@
 package com.javabase.concurrent.synchronizer.test023;
-
-import javax.smartcardio.CommandAPDU;
-
 /**
  * @Author: wtshen
  * @Description:
  * @Date: Created in 下午8:43 18/3/26.
  * @Modified By:
  */
-public class OutClass {
+public class OuterClass1 {
     public void display() {
-        System.out.println("this is OutClass");
+        System.out.println("this is OuterClass1");
     }
 
     /**
@@ -30,19 +27,19 @@ public class OutClass {
             System.out.println("this is InnerClass");
         }
 
-        public OutClass getOutClass() {
-            return OutClass.this;
+        public OuterClass1 getOutClass() {
+            return OuterClass1.this;
         }
     }
 
     public static void main(String[] args) {
-        OutClass outClass = new OutClass();
-        outClass.display();
+        OuterClass1 outerClass1 = new OuterClass1();
+        outerClass1.display();
 
         // 创建一个内部类对象，必须利用outerClass.new
-        OutClass.InnerClass innerClass = outClass.new InnerClass();
+        OuterClass1.InnerClass innerClass = outerClass1.new InnerClass();
         innerClass.display();
 
-        outClass.displayInnerClass();
+        outerClass1.displayInnerClass();
     }
 }

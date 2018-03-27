2d9d9e66da4d377cb65647e1632b37fa29b9f4f1
wtshen <hiswt@sina.com>
2018-03-27 22:11:27 +0800

    新增ConcurrentQueue示例

--- src/main/java/com/javabase/concurrent/container/ConcurrentQueue.java
+++ src/main/java/com/javabase/concurrent/container/ConcurrentQueue.java
@@ -4,9 +4,7 @@ import com.google.common.collect.Lists;
 
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**

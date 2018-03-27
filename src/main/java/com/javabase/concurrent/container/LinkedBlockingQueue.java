08142b5aa2f7f58bd849bbd6f74eda4424522e6c
wtshen <hiswt@sina.com>
2018-03-27 23:24:52 +0800

    新增LinkedBlockQueue示例

---
+++ src/main/java/com/javabase/concurrent/container/LinkedBlockingQueue.java
@@ -0,0 +1,41 @@
+package com.javabase.concurrent.container;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: wtshen
+ * @Description:
+ * @Date: Created in 下午10:54 18/3/27.
+ * @Modified By:
+ */
+public class LinkedBlockingQueue {
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new java.util.concurrent.LinkedBlockingQueue<>();
+        Random random = new Random();
+
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    queue.put("a" + i);
+                    TimeUnit.MILLISECONDS.sleep(random.nextInt(1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + "--" + queue.take());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}

36c099fad40760f45a5f74301f82c369eca30bed
wtshen <hiswt@sina.com>
2018-03-28 19:49:47 +0800

    新增LinkedTransferQueue示例

--- src/main/java/com/javabase/concurrent/container/LinkedTransferQueueDemo.java
+++ src/main/java/com/javabase/concurrent/container/LinkedTransferQueueDemo.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
  * @Author: wtshen
  * @Description: 1.LinkedTransferQueue实际上是ConcurrentLinkedQueue、SynchronousQueue（公平模式）和LinkedBlockingQueue的超集。
  * 而且LinkedTransferQueue更好用，因为它不仅仅综合了这几个类的功能，同时也提供了更高效的实现。
- * 2.当我们不想生产者过度生产消息时，TransferQueue可能非常有用，因为没有消费者时队列就阻塞,不会继续生产,
+ * 2.当我们不想生产者过度生产消息时，TransferQueue可能非常有用，利用transfer()可以在没有消费者时队列就阻塞,不会继续生产,
  * 可避免发生OutOfMemory错误。在这样的设计中，消费者的消费能力将决定生产者产生消息的速度。
  * @Date: Created in 下午7:33 18/3/28.
  * @Modified By:
@@ -32,6 +32,7 @@ public class LinkedTransferQueueDemo {
             for (int i = 0; i < 3; i++) {
                 try {
                     System.out.println("Producer is waiting to transfer...");
+                    // 利用put\add不会阻塞队列
                     lnkTransQueue.transfer("A" + i);
                     System.out.println("producer transfered element: A" + i);
                 } catch (InterruptedException e) {

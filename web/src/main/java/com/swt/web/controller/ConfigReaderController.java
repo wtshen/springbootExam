7831d391d4c48994592c304e32844e2171ac79a1
wtshen <wtshen@ctrip.com>
2018-06-01 17:35:02 +0800

    调整springboot启动无法访问controller问题

--- web/src/main/java/com/swt/web/controller/ConfigReaderController.java
+++ web/src/main/java/com/swt/web/controller/ConfigReaderController.java
@@ -3,15 +3,17 @@ package com.swt.web.controller;
 import com.swt.web.viewmodel.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 /**
+ * 读取配置文件中属性
  * Created by wtshen on 18/2/17.
  */
 @RestController
-public class MainController {
+public class ConfigReaderController {
 
     @Value("${username}")
     private String userName;
@@ -19,17 +21,16 @@ public class MainController {
     @Value("${userage}")
     private Integer userAge;
 
-
     @Autowired
     private UserInfo userInfo;
 
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/index")
     public String hello() {
         return String.format("%s:%d", userName, userAge);
     }
 
-   /* @GetMapping(value = "/hello3")
+    @GetMapping(value = "/hello")
     public String hello1() {
         return String.format("%s:%d", userInfo.getUserName(), userInfo.getUserAge());
-    }*/
+    }
 }

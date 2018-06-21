912a3694708ca79d560e3acb0e2155e763a26eb5
wtshen <wtshen@ctrip.com>
2018-06-21 17:34:23 +0800

    新增常量类

---
+++ common/src/main/java/com/swt/Constants.java
@@ -0,0 +1,11 @@
+package com.swt;
+
+/**
+ * @Author: wtshen
+ * @Description:
+ * @Date: Created in 14:03 2018/6/21
+ * @Modified By:
+ */
+public final class Constants {
+    public final static String userName = "shenwentao";
+}

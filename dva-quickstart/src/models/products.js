2b7f77fc3de6c3bb8454dc39b1978776692da91f
wtshen <wtshen@ctrip.com>
2018-07-26 10:56:30 +0800

    移除dva相关内容

--- dva-quickstart/src/models/products.js
+++
@@ -1,9 +0,0 @@
-export default {
-    namespace: 'aaa',
-    state: [],
-    reducers: {
-        'delete'(state, { payload: id }) {
-            return state.filter(item => item.id !== id);
-        },
-    },
-};

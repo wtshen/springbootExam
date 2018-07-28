765133a126e9ee538a160605ed65b8e41cd79c8c
wtshen <hiswt@sina.com>
2018-07-28 22:03:39 +0800

    新增 antd Form 示例

--- reactDemo/antd-demo/src/App.js
+++ reactDemo/antd-demo/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'antd';
+import AdvancedSearchForm from './containers/AdvancedSearchForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type='primary'>Button</Button>
+        <AdvancedSearchForm />
+        {/* <Button type='primary'>Button</Button> */}
       </div>
     );
   }

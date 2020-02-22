e3b2c9b84fbe7ef9a4a4e9ac2061a8f9d68395f6
wtshen <hiswt@sina.com>
2020-02-22 08:12:42 +0800

    新增spring创建bean示例

--- springdemo/src/main/java/spring/annotation/UserService.java
+++ springdemo/src/main/java/spring/annotation/UserService.java
@@ -10,8 +10,11 @@ import javax.annotation.Resource;
  * @Date: Created in 7:49 AM 2020/2/22.
  * @Modified By:
  */
-@Component
+@Component // 在IOC容器中加入对象名称与类名相同，首字母小写
 public class UserService {
+    /**
+     *  属性注入，写在字段上
+     */
     @Resource
     private UserDao userDao;
 

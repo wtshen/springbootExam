0f4cf51b91d6025c3bb03dc9620eb93e842834ae
wtshen <hiswt@sina.com>
2020-03-07 22:58:41 +0800

    修改工厂创建bean

--- springdemo/src/main/java/spring/ioc/facotry/PersonTest.java
+++ springdemo/src/main/java/spring/ioc/facotry/PersonTest.java
@@ -11,6 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class PersonTest {
     public static void main(String[] args){
+        System.out.println("--静态工厂方法");
         // 静态工厂方法
         ApplicationContext context = new ClassPathXmlApplicationContext("factory.xml");
         Chinese chinese = (Chinese) context.getBean("chinese");
@@ -18,10 +19,11 @@ public class PersonTest {
         American american = (American) context.getBean("american");
         american.say();
 
+        System.out.println("--实例工厂方法");
         // 实例工厂方法
-        Chinese chinese1 = (Chinese) context.getBean("chinese");
+        Chinese chinese1 = (Chinese) context.getBean("ch");
         chinese1.say();
-        American american1 = (American) context.getBean("american");
+        American american1 = (American) context.getBean("us");
         american1.say();
     }
 }
